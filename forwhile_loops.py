3ccff18f85f6ebe70c2d80bdd0939c45fcb2b2bd
quangwar@gmail.com <quangwar@gmail.com>
2019-08-05 23:55:10 +0700

    Add ex2

--- forwhile_loops.py
+++ forwhile_loops.py
@@ -9,7 +9,21 @@ def ex1():
     num_faces = [0, 0, 0, 0, 0, 0]
 
     for _ in range(total_roll):
-        n = random.randint(0,5)
+        n = random.randint(0, 5)
         num_faces[n] = num_faces[n] + 1
 
     return num_faces
+
+def ex2():
+    """
+    Flip a coin with 70% rate that will be head side and 30% that will be tail side
+    :return: a list contains 2 numbers show how many times the coin's side appear
+    """
+    total_flip = 1000
+    num_sides = [0, 0]
+
+    for _ in range(total_flip):
+        n = random.random()
+        num_sides[round(n)] = num_sides[round(n)] + 1
+
+    return num_sides
